cc31e157f8576302efad4ff130dc913f960b6aa1
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-10-06 19:05:21 +0200

    add class Peli

---
+++ GestionCine/src/main/Pelicula.java
@@ -0,0 +1,49 @@
+package main;
+
+public class Pelicula {
+	
+	private String titulo;
+	private int duracion;
+	private int edad_minima;
+	private String director;
+	
+	public Pelicula(String titulo, int duracion, int edad_minima, String director) {
+		this.titulo = titulo;
+		this.duracion = duracion;
+		this.edad_minima = edad_minima;
+		this.director = director;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public int getEdad_minima() {
+		return edad_minima;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	public void setEdad_minima(int edad_minima) {
+		this.edad_minima = edad_minima;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	
+}
--- GestionCine/src/main/mainApp.java
+++ GestionCine/src/main/mainApp.java
@@ -3,7 +3,7 @@ package main;
 public class mainApp {
 
 	public static void main(String[] args) {
-	  System.out.println("C");
+	  
 	}
 
 }
