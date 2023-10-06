e118051110bea41415b0efe2a5722c02f14b5ae4
Robert <robertrlm16@gmail.com>
2023-10-06 18:55:30 +0200

    Clase Espectador

--- GestionCine/src/main/mainApp.java
+++ GestionCine/src/main/mainApp.java
@@ -2,7 +2,8 @@ package main;
 
 public class mainApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		System.out.print("Ingresa el precio unitario del producto: ");
 
